31a32ecda88a71b389d833fce829df9356ad2218
Robert Wild <wildr2@rpi.edu>
2014-09-28 10:58:55 -0400

    Spinner track fix

--- UnityTestSpace/Assets/Scripts/Spinner Game/Spinner.cs
+++ UnityTestSpace/Assets/Scripts/Spinner Game/Spinner.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Spinner : MonoBehaviour 
 {
     private bool on_track = false;
+    Collider2D track = null;
     private Vector2 track_direction;
 
     public Material mat_on_track, mat_normal;
@@ -35,16 +36,16 @@ public class Spinner : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.tag == "track")
+        if (collider.tag == "track" && collider == track) // only detatch if not on a different track now
         {
-            DetachFromTrack();
+            DetatchFromTrack();
         }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         foreach (ContactPoint2D contact in collision.contacts)
         {
-            if (contact.collider.tag == "track")
+            if (contact.collider.tag == "track" && contact.collider != track) // only attach to new tracks
             {
                 AttachToTrack(contact);
             }
@@ -55,6 +56,7 @@ public class Spinner : MonoBehaviour
     private void AttachToTrack(ContactPoint2D contact)
     {
         on_track = true;
+        track = contact.collider;
         sprite_renderer.material = mat_on_track;
 
         rigidbody2D.velocity = Vector2.zero;
@@ -62,9 +64,10 @@ public class Spinner : MonoBehaviour
         Vector2 normal = (contact.point - (Vector2)transform.position).normalized;
         track_direction = new Vector2(normal.y, -normal.x);
     }
-    private void DetachFromTrack()
+    private void DetatchFromTrack()
     {
         on_track = false;
+        track = null;
         sprite_renderer.material = mat_normal;
 
         rigidbody2D.velocity = track_direction * track_speed;
