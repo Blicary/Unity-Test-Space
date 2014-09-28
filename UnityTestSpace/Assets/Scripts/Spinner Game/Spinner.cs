143fd18393fc2489b74b374335a854b5df9360cf
Robert Wild <wildr2@rpi.edu>
2014-09-28 12:38:48 -0400

    spinner track improvements

--- UnityTestSpace/Assets/Scripts/Spinner Game/Spinner.cs
+++ UnityTestSpace/Assets/Scripts/Spinner Game/Spinner.cs
@@ -10,6 +10,9 @@ public class Spinner : MonoBehaviour
     public Material mat_on_track, mat_normal;
     private SpriteRenderer sprite_renderer;
 
+    public LayerMask tracks_layer;
+    public float radius = 1f;
+
     private float track_speed = 20f;
 
 
@@ -27,43 +30,83 @@ public class Spinner : MonoBehaviour
     public void Update()
     {
         if (on_track)
-            transform.Translate(track_direction * track_speed * Time.deltaTime);
-        else
         {
-            rigidbody2D.velocity *= 0.99f;
+            UpdateTrackAttatchment();
+            transform.Translate(track_direction * track_speed * Time.deltaTime);
         }
     }
+    public void FixedUpdate()
+    {
+        if (!on_track)
+            rigidbody2D.velocity *= 1f - (0.7f * Time.fixedDeltaTime);
+    }
 
-    private void OnTriggerExit2D(Collider2D collider)
+    /*
+    public void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.tag == "track" && collider == track) // only detatch if not on a different track now
         {
+            Debug.Log("leave track");
             DetatchFromTrack();
         }
+
+        Debug.Log("leave old track (do nothing)");
     }
+     * */
+    
     public void OnCollisionEnter2D(Collision2D collision)
     {
         foreach (ContactPoint2D contact in collision.contacts)
         {
-            if (contact.collider.tag == "track" && contact.collider != track) // only attach to new tracks
+            // attach to a track if not on a track already
+            if (!on_track && contact.collider.tag == "track")
             {
                 AttachToTrack(contact);
             }
         }
     }
     
+    
+    private void UpdateTrackAttatchment()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Perpendicular(track_direction), radius + 0.5f, tracks_layer);
 
-    private void AttachToTrack(ContactPoint2D contact)
+        // detach
+        if (hit.collider == null)
+        {
+            DetatchFromTrack();
+            return;
+        }
+        
+        // attach to new track (segment)
+        if (hit.collider != track) // new track
+            AttachToTrack(hit.collider);
+
+        // update spinner position (keep next to track)
+        transform.position = hit.point + hit.normal * radius;
+
+        // update direction
+        track_direction = -Perpendicular(hit.normal);
+        //Debug.DrawLine(transform.position, hit.point, Color.blue);
+        //Debug.DrawLine(transform.position, (Vector2)transform.position + track_direction * radius, Color.white);
+    }
+    private void AttachToTrack(Collider2D collider)
     {
         on_track = true;
-        track = contact.collider;
+        track = collider;
         sprite_renderer.material = mat_on_track;
 
         rigidbody2D.velocity = Vector2.zero;
-
-        Vector2 normal = (contact.point - (Vector2)transform.position).normalized;
-        track_direction = new Vector2(normal.y, -normal.x);
     }
+    private void AttachToTrack(ContactPoint2D contact)
+    {
+        AttachToTrack(contact.collider);
+
+        // find track_direction first time
+        Vector2 normal = (contact.point - (Vector2)transform.position).normalized;
+        track_direction = Perpendicular(normal);
+    }
+    
     private void DetatchFromTrack()
     {
         on_track = false;
@@ -71,6 +114,12 @@ public class Spinner : MonoBehaviour
         sprite_renderer.material = mat_normal;
 
         rigidbody2D.velocity = track_direction * track_speed;
+    }
+
+
+    private Vector2 Perpendicular(Vector2 v)
+    {
+        return new Vector2(v.y, -v.x);
     }
 
 }
